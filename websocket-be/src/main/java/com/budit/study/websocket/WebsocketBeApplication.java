def67547a448fb618fdcbf22174ab187011fce5f
Ligno We <ligno@bud-it.com>
2022-05-09 14:04:43 +0900

    Init.

---
+++ websocket-be/src/main/java/com/budit/study/websocket/WebsocketBeApplication.java
@@ -0,0 +1,13 @@
+package com.budit.study.websocket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebsocketBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebsocketBeApplication.class, args);
+    }
+
+}
---
+++ websocket-be/src/test/java/com/budit/study/websocket/WebsocketBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.budit.study.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsocketBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
